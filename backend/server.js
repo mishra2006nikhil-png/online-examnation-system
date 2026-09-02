942e06c5875d5edb28bd8d552270e0d07dea414b
mishra2006nikhil-png <mishra2006nikhil@gmail.com>
2026-09-02 15:26:51 +0530

    Update server.js
    Update server port for Render

--- backend/server.js
+++ backend/server.js
@@ -479,6 +479,8 @@ app.post("/admin-login", (req, res) => {
     });
 
 });
-app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
