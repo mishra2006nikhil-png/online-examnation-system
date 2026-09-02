661fea0e6da76ca93119d920a76c12e0c9961301
mishra2006nikhil-png <mishra2006nikhil@gmail.com>
2026-09-02 21:24:44 +0530

    Update server.js
    fix frontend serving

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
